dd2cfe12473289710a846a7fd828dce698d7767c
Sebastian <Sebastian@DESKTOP-FGHKP6U>
2023-04-20 19:07:53 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace WarehouseManager
+{
+    partial class WarehouseManager
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.warehouseGridView = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.programVersionLabel = new System.Windows.Forms.Label();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.dashboardLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.warehouseGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(56, 61);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(85, 35);
+            this.newButton.TabIndex = 0;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(147, 61);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(85, 35);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(238, 61);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(85, 35);
+            this.deleteButton.TabIndex = 2;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(470, 74);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idTextBox.TabIndex = 3;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(576, 74);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(682, 74);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.priceTextBox.TabIndex = 5;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(788, 74);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.quantityTextBox.TabIndex = 6;
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(894, 74);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.descriptionTextBox.TabIndex = 7;
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Items.AddRange(new object[] {
+            "Hardware"});
+            this.categoryComboBox.Location = new System.Drawing.Point(1000, 74);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(120, 21);
+            this.categoryComboBox.TabIndex = 8;
+            // 
+            // warehouseGridView
+            // 
+            this.warehouseGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.warehouseGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.warehouseGridView.Location = new System.Drawing.Point(56, 102);
+            this.warehouseGridView.Name = "warehouseGridView";
+            this.warehouseGridView.ReadOnly = true;
+            this.warehouseGridView.Size = new System.Drawing.Size(1064, 454);
+            this.warehouseGridView.TabIndex = 9;
+            this.warehouseGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.warehouseGridView_CellDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(41)))), ((int)(((byte)(73)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(41, 580);
+            this.panel1.TabIndex = 10;
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.exitButton.FlatAppearance.BorderSize = 0;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.exitButton.ForeColor = System.Drawing.Color.Transparent;
+            this.exitButton.Location = new System.Drawing.Point(1108, 0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(30, 25);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "X";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // programVersionLabel
+            // 
+            this.programVersionLabel.AutoSize = true;
+            this.programVersionLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.programVersionLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.programVersionLabel.Location = new System.Drawing.Point(825, 559);
+            this.programVersionLabel.Name = "programVersionLabel";
+            this.programVersionLabel.Size = new System.Drawing.Size(295, 14);
+            this.programVersionLabel.TabIndex = 12;
+            this.programVersionLabel.Text = "Warehouse Manager  ver: 0.1, author: Sebastian Stępień";
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.idLabel.Location = new System.Drawing.Point(467, 57);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(65, 14);
+            this.idLabel.TabIndex = 13;
+            this.idLabel.Text = "Product ID:";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.nameLabel.Location = new System.Drawing.Point(573, 59);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(41, 14);
+            this.nameLabel.TabIndex = 14;
+            this.nameLabel.Text = "Name:";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priceLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.priceLabel.Location = new System.Drawing.Point(679, 57);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(35, 14);
+            this.priceLabel.TabIndex = 15;
+            this.priceLabel.Text = "Price:";
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantityLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.quantityLabel.Location = new System.Drawing.Point(785, 57);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(55, 14);
+            this.quantityLabel.TabIndex = 16;
+            this.quantityLabel.Text = "Quantity:";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descriptionLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.descriptionLabel.Location = new System.Drawing.Point(891, 57);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(69, 14);
+            this.descriptionLabel.TabIndex = 17;
+            this.descriptionLabel.Text = "Description:";
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoryLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.categoryLabel.Location = new System.Drawing.Point(997, 57);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(57, 14);
+            this.categoryLabel.TabIndex = 18;
+            this.categoryLabel.Text = "Category:";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(79)))), ((int)(((byte)(99)))));
+            this.searchTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.searchTextBox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.searchTextBox.Location = new System.Drawing.Point(852, 20);
+            this.searchTextBox.Multiline = true;
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(240, 20);
+            this.searchTextBox.TabIndex = 19;
+            this.searchTextBox.Text = "Search for something...";
+            // 
+            // dashboardLabel
+            // 
+            this.dashboardLabel.AutoSize = true;
+            this.dashboardLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboardLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(176)))));
+            this.dashboardLabel.Location = new System.Drawing.Point(50, 9);
+            this.dashboardLabel.Name = "dashboardLabel";
+            this.dashboardLabel.Size = new System.Drawing.Size(138, 31);
+            this.dashboardLabel.TabIndex = 20;
+            this.dashboardLabel.Text = "Dashboard";
+            // 
+            // WarehouseManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1133, 580);
+            this.Controls.Add(this.dashboardLabel);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.categoryLabel);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.programVersionLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.warehouseGridView);
+            this.Controls.Add(this.categoryComboBox);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.newButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WarehouseManager";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Warehouse Management";
+            this.Load += new System.EventHandler(this.WarehouseManager_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.warehouseGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.DataGridView warehouseGridView;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label programVersionLabel;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label dashboardLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WarehouseManager
+{
+    public partial class WarehouseManager : Form
+    {
+        DataTable warehouse = new DataTable();
+        public WarehouseManager()
+        {
+            InitializeComponent();
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            idTextBox.Text = "";
+            nameTextBox.Text = "";
+            priceTextBox.Text = "";
+            quantityTextBox.Text = "";
+            descriptionTextBox.Text = "";
+            categoryComboBox.SelectedIndex = -1;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            // Save all the values from our fields into variables
+            String id = idTextBox.Text;
+            String name = nameTextBox.Text;
+            String price = priceTextBox.Text;
+            String quantity = quantityTextBox.Text;
+            String description = descriptionTextBox.Text;
+            String category = (string)categoryComboBox.SelectedItem;
+
+            // Add these values to the datatable
+            warehouse.Rows.Add(id, name, price, quantity, description, category);
+
+            // Clear fields after save
+            newButton_Click(sender, e);
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                warehouse.Rows[warehouseGridView.CurrentCell.RowIndex].Delete();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error: " + err);
+            }
+        }
+
+        private void warehouseGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                idTextBox.Text = warehouse.Rows[warehouseGridView.CurrentCell.RowIndex].ItemArray[0].ToString();
+                nameTextBox.Text = warehouse.Rows[warehouseGridView.CurrentCell.RowIndex].ItemArray[1].ToString();
+                priceTextBox.Text = warehouse.Rows[warehouseGridView.CurrentCell.RowIndex].ItemArray[3].ToString();
+                quantityTextBox.Text = warehouse.Rows[warehouseGridView.CurrentCell.RowIndex].ItemArray[4].ToString();
+                descriptionTextBox.Text = warehouse.Rows[warehouseGridView.CurrentCell.RowIndex].ItemArray[5].ToString();
+
+                String itemToLookFor = warehouse.Rows[warehouseGridView.CurrentCell.RowIndex].ItemArray[2].ToString();
+                categoryComboBox.SelectedIndex = categoryComboBox.Items.IndexOf(itemToLookFor);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("There has been an error: " + err);
+            }
+        }
+
+        private void WarehouseManager_Load(object sender, EventArgs e)
+        {
+            warehouse.Columns.Add("ID");
+            warehouse.Columns.Add("Name");
+            warehouse.Columns.Add("Price");
+            warehouse.Columns.Add("Quantity");
+            warehouse.Columns.Add("Description");
+            warehouse.Columns.Add("Category");
+
+            warehouseGridView.DataSource = warehouse;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
